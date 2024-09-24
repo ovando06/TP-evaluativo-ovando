d7b56bf7c04ca2fafacabdeb5592671bcb0baf23
ovando06 <lunaovando65@gmail.com>
2024-09-24 11:08:45 -0300

    agreguè ngIf en la categorìa 'mantenimiento y reparaciones', estilos en el navbar, arreglè el modo oscuro

--- src/app/modules/producto/pages/servicios/mantenimiento/mantenimiento.component.ts
+++ src/app/modules/producto/pages/servicios/mantenimiento/mantenimiento.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./mantenimiento.component.css']
 })
 export class MantenimientoComponent {
+  mostrar1 = false;
+  mostrar2 = false;
+  mostrar3 = false;
+  mostrar4 = false;
+  
+
+
 
 }
